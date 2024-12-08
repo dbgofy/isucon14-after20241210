e2bd3851b809226d9fe692211e00df508f87f95c
bgpat <bgpat23@gmail.com>
2024-12-08 11:59:30 +0900

    Reduce for loop

--- home/isucon/webapp/go/app_handlers.go
+++ home/isucon/webapp/go/app_handlers.go
@@ -883,6 +883,7 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 			LEFT JOIN ride_statuses AS rs
 							ON rs.ride_id = r.id
 							AND status = 'COMPLETED'
+			WHERE c.is_active
 			GROUP BY c.id, c.owner_id, c.name, c.model, c.is_active, c.access_token, c.created_at, c.updated_at
 			HAVING COUNT(r.id) - COUNT(rs.id) = 0
 		`,
@@ -894,33 +895,6 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 
 	nearbyChairs := []appGetNearbyChairsResponseChair{}
 	for _, chair := range chairs {
-		if !chair.IsActive {
-			continue
-		}
-
-		rides := []*Ride{}
-		if err := tx.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id = ? ORDER BY created_at DESC`, chair.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-
-		skip := false
-		for _, ride := range rides {
-			// 過去にライドが存在し、かつ、それが完了していない場合はスキップ
-			status, err := getLatestRideStatus(ctx, tx, ride.ID)
-			if err != nil {
-				writeError(w, http.StatusInternalServerError, err)
-				return
-			}
-			if status != "COMPLETED" {
-				skip = true
-				break
-			}
-		}
-		if skip {
-			continue
-		}
-
 		// 最新の位置情報を取得
 		chairLocation := &ChairLocation{}
 		err = tx.GetContext(
