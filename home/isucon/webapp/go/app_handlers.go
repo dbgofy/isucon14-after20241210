efb8d34849a46b1f61a8ca7bbf4bd5f7541f908d
bgpat <bgpat23@gmail.com>
2024-12-11 03:05:13 +0900

    Revert tick

--- home/isucon/webapp/go/app_handlers.go
+++ home/isucon/webapp/go/app_handlers.go
@@ -671,7 +671,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 		f.Flush()
 	}
 
-	ticker := time.NewTicker(time.Second * 10)
+	ticker := time.NewTicker(time.Second * 1)
 
 	first := make(chan (struct{}), 1)
 	first <- struct{}{}
