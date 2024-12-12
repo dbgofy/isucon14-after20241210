496e3c417d62e9b21f671f66f4f69799cfc3a05d
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-12-13 02:10:41 +0900

    COMPLETEDになったら次のマッチングをできるようにする

--- home/isucon/webapp/go/app_handlers.go
+++ home/isucon/webapp/go/app_handlers.go
@@ -637,6 +637,7 @@ func appPostRideEvaluatation(w http.ResponseWriter, r *http.Request) {
 		slog.Error("failed to send notification", "error", err)
 		return
 	}
+	matchingChannel <- ride.ChairID.String
 
 	if err := tx.Commit(); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
