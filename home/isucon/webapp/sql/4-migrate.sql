d51307f084d3611fd231659b7aff5d81490aa72a
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-12-13 03:37:03 +0900

    index調整

--- home/isucon/webapp/sql/4-migrate.sql
+++ home/isucon/webapp/sql/4-migrate.sql
@@ -3,8 +3,9 @@ ALTER TABLE ride_statuses ADD INDEX IX_ride_statuses_ride_id_chair_sent_at_creat
 ALTER TABLE chair_locations ADD INDEX IX_chair_locations_chair_id_created_at (chair_id, created_at);
 ALTER TABLE chair_locations ADD INDEX IX_chair_locations_chair_id_id (chair_id, id);
 ALTER TABLE rides ADD INDEX IX_rides_chair_id_updated_at (chair_id, updated_at);
-ALTER TABLE rides ADD INDEX IX_rides_evaluation (evaluation);
+ALTER TABLE rides ADD INDEX IX_rides_evaluation_chair_id_updated_at (evaluation, chair_id, updated_at);
 ALTER TABLE chairs ADD INDEX IX_chairs_access_token (access_token);
+ALTER TABLE chairs ADD INDEX IX_chairs_owner_id (owner_id);
 
 DROP TABLE IF EXISTS chair_locations_minus_distance;
 CREATE TABLE chair_locations_minus_distance
