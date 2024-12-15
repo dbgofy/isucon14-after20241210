e055c0066c1a8afb16aacf6250124e60da4663f6
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-12-15 09:06:25 +0900

    txで囲ってあげる

--- home/isucon/webapp/go/internal_handlers.go
+++ home/isucon/webapp/go/internal_handlers.go
@@ -183,14 +183,19 @@ func matchingComp(ctx context.Context, rides []Ride) error {
 	for _, v := range rideIDs {
 		b = append(b, v)
 	}
-	_, err := db.ExecContext(ctx, fmt.Sprintf("UPDATE rides SET chair_id = ELT(FIELD(id%s)%s) WHERE id IN (?%s)", strings.Repeat(",?", len(rideIDs)), strings.Repeat(",?", len(chairIDs)), strings.Repeat(",?", len(rideIDs)-1)), b...)
+	tx, err := db.Beginx()
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+	defer tx.Rollback()
+	_, err = tx.ExecContext(ctx, fmt.Sprintf("UPDATE rides SET chair_id = ELT(FIELD(id%s)%s) WHERE id IN (?%s)", strings.Repeat(",?", len(rideIDs)), strings.Repeat(",?", len(chairIDs)), strings.Repeat(",?", len(rideIDs)-1)), b...)
 	if err != nil {
 		slog.Error("failed to update ride", "error", err)
 		return fmt.Errorf("failed to update ride: %w", err)
 	}
 
 	for _, ride := range rides {
-		err := sendAppGetNotificationChannel(ctx, nil, "MATCHING", &ride)
+		err := sendAppGetNotificationChannel(ctx, tx, "MATCHING", &ride)
 		if err != nil {
 			slog.Error("failed to send notification", "error", err)
 			return fmt.Errorf("failed to send notification: %w", err)
@@ -200,6 +205,9 @@ func matchingComp(ctx context.Context, rides []Ride) error {
 			slog.Error("failed to send notification", "error", err)
 			return fmt.Errorf("failed to send notification: %w", err)
 		}
+	}
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
 	}
 	return nil
 }
